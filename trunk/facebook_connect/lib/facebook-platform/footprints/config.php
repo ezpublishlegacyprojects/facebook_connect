267f001830deac07e93d15a41a616e26d3fcfc48
andre1 <info@example.com>
2010-02-04 22:44:12 +0000

    - Updated facebook platform files ( 21. Jan 2010 version )
    git-svn-id: http://svn.projects.ez.no/facebook_connect@14 d2e6c3fc-9711-4192-b327-2ff0569793f3

--- trunk/facebook_connect/lib/facebook-platform/footprints/config.php
+++ trunk/facebook_connect/lib/facebook-platform/footprints/config.php
@@ -1,21 +1,21 @@
 <?php
 
 // Get these from http://developers.facebook.com
-$api_key = 'b72334f41c909bead38d2d490905e6b0';
-$secret  = '1bfe5a2a2a486a522998c4008f675c75';
+$api_key = 'YOUR_API_KEY';
+$secret  = 'YOUR_SECRET';
 /* While you're there, you'll also want to set up your callback url to the url
  * of the directory that contains Footprints' index.php, and you can set the
  * framed page URL to whatever you want.  You should also swap the references
  * in the code from http://apps.facebook.com/footprints/ to your framed page URL. */
 
 // The IP address of your database
-$db_ip = 'localhost';           
+$db_ip = '10.0.0.0';           
 
-$db_user = 'ezp';
-$db_pass = 'ezpezp';
+$db_user = 'root';
+$db_pass = 'YOUR_DB_PASSWORD';
 
 // the name of the database that you create for footprints.
-$db_name = 'sf_volano_simon_experimental';
+$db_name = 'footprints';
 
 /* create this table on the database:
 CREATE TABLE `footprints` (
@@ -26,4 +26,3 @@ CREATE TABLE `footprints` (
   KEY `to` (`to`)
 )
 */
-?>
